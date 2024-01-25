4b05ebef2d9e97f83164dcb87a28482198d8db4a
Irfan Syarifudin <92904571+Irfan3006@users.noreply.github.com>
2024-01-25 18:59:55 +0700

    fix minor bug

--- CRUD Mata Pelajaran/index.php
+++ CRUD Mata Pelajaran/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>CRUD DATA Siswa - SMK Negeri 3 Yogyakarta</title>
-    <link rel="icon" type="image/x-icon" href="favicon.png">
+    <title>CRUD DATA Jadwal Pelajaran - SMK Negeri 3 Yogyakarta</title>
+    <link rel="stylesheet" href="flavicon.png">    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         .margin-custom a {
